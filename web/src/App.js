0ad1c2f9f3bc620aecaa02c34943b859f4b1fa4e
Thiago Seixas <thiago.seixas10@gmail.com>
2018-07-10 16:23:15 -0400

    resposive blocks

--- web/src/App.js
+++ web/src/App.js
@@ -119,7 +119,33 @@ class App extends Component {
       infinite: false,
       speed: 1000,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     return (
